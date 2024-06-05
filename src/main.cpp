f175ecf73d8088b2185127a7ae3ee92a88bdf28c
Matt <43940868+MSmithDev@users.noreply.github.com>
2024-06-05 15:38:16 -0400

    needs sensor data

--- src/main.cpp
+++ src/main.cpp
@@ -25,34 +25,41 @@
 #include <Arduino.h>
 #include <BleGamepad.h>
 
-BleGamepad bleGamepad;
+BleGamepad bleGamepad("MDS-IMUJoy", "MSmithDev", 100);
+BleGamepadConfiguration bleGamepadConfig;
+
+const int reportInterval = 5; // ms
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Starting BLE work!");
+    //Serial.begin(115200);
+    //Serial.println("Starting BLE work!");
+    
+
     bleGamepad.begin();
-    // The default bleGamepad.begin() above enables 16 buttons, all axes, one hat, and no simulation controls or special buttons
 }
+
+
+int rx = 0;
+int ry = 0;
+int rz = 0;
 
 void loop()
 {
     if (bleGamepad.isConnected())
     {
-        Serial.println("Press buttons 5, 16 and start. Move all enabled axes to max. Set DPAD (hat 1) to down right.");
-        bleGamepad.press(BUTTON_5);
-        bleGamepad.press(BUTTON_16);
-        bleGamepad.pressStart();
-        bleGamepad.setAxes(32767, 32767, 32767, 32767, 32767, 32767, 32767, 32767);
-        bleGamepad.setHat1(HAT_DOWN_RIGHT);
-        // All axes, sliders, hats etc can also be set independently. See the IndividualAxes.ino example
-        delay(500);
+        
+        rx += 10;
+        if (rx > 32767)
+        {
+            rx = 0;
+        }
+        //Serial.print("RX: ");
+        //Serial.println(rx);
 
-        Serial.println("Release button 5 and start. Move all axes to min. Set DPAD (hat 1) to centred.");
-        bleGamepad.release(BUTTON_5);
-        bleGamepad.releaseStart();
-        bleGamepad.setHat1(HAT_CENTERED);
-        bleGamepad.setAxes(0, 0, 0, 0, 0, 0, 0, 0);
-        delay(500);
+        
+        bleGamepad.setAxes(0, 0, 0, rx, rx, rx, 0, 0);
+        
     }
+    delay(reportInterval);
 }
